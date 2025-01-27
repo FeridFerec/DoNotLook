17b22b47794af0f7cd9284adccb25e185ad491fd
Ferid Ferec <167343636+FeridFerec@users.noreply.github.com>
2025-01-27 23:36:26 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let a = document.querySelectorAll("a");
+
+
+a.forEach((an) => {
+    an.addEventListener("click",() => {
+        alert("Bunun ucun melumatlari admine vermelsiz baxib fix eder")
+    });
+});
